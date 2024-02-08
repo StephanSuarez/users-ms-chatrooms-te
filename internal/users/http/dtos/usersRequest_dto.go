0e5b5be048082e026a7b003b92e04461c16f9180
Chespide <84825158+Chespide@users.noreply.github.com>
2024-02-08 11:02:48 -0500

    mappers

--- internal/users/http/dtos/usersRequest_dto.go
+++ internal/users/http/dtos/usersRequest_dto.go
@@ -1,9 +1,31 @@
 package dtos
 
+import (
+	"users/internal/users/entity"
+)
+
 type UsersRequestDTO struct {
 	Name     string `json:"name"`
 	LastName string `json:"lastName"`
 	UserName string `json:"userName"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+func (urd *UsersRequestDTO) MapEntityToDto(userEntity *entity.Users) {
+	urd.Name = userEntity.Name
+	urd.LastName = userEntity.LastName
+	urd.UserName = userEntity.UserName
+	urd.Email = userEntity.Email
+	urd.Password = userEntity.Password
+}
+
+func (urd *UsersRequestDTO) MapEntityFromDto() *entity.Users {
+	return &entity.Users{
+		Name:     urd.Name,
+		LastName: urd.LastName,
+		UserName: urd.UserName,
+		Email:    urd.Email,
+		Password: urd.Password,
+	}
 }
--- internal/users/repository/models/users.go
+++ internal/users/repository/models/users.go
@@ -1,9 +1,29 @@
 package models
 
+import "users/internal/users/entity"
+
 type Users struct {
 	Name     string `bson:"name"`
 	LastName string `bson:"lastName"`
 	UserName string `bson:"userName"`
 	Email    string `bson:"email"`
 	Password string `bson:"password"`
+}
+
+func (model *Users) MapEntityToModel(userEntity *entity.Users) {
+	model.Name = userEntity.Name
+	model.LastName = userEntity.LastName
+	model.UserName = userEntity.UserName
+	model.Email = userEntity.Email
+	model.Password = userEntity.Password
+}
+
+func (model *Users) MapEntityFromModel() *entity.Users {
+	return &entity.Users{
+		Name:     model.Name,
+		LastName: model.LastName,
+		UserName: model.UserName,
+		Email:    model.Email,
+		Password: model.Password,
+	}
 }
